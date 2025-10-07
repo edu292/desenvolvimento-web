91ccf110e0d31b4731ce25599e81b96cb379f148
edu292 <eduskoroboatei@gmail.com>
2025-10-07 11:48:52 -0300

    alimento atualizado

--- js/alimento.js
+++ js/alimento.js
@@ -6,35 +6,32 @@ if (!validarSessao()) {
     window.location.assign('../index.html');
 }
 
+const corpoTabela = document.getElementById('corpo-tabela');
+const dbAlimento = JSON.parse(localStorage.getItem('dbAlimento')) ?? []
 
-const tempAlimento = {
-    Alimento: "Feijão",
-    Categoria: "Perecível",
-    Descrição: "Pacote de feijão 1kg",
-    Restrição: "Contém glúten",
-    Validade: "22/02/2026",
-    Armazenamento: "Local arejado e seco"
+
+function carregarItens(){
+    if (dbAlimento && dbAlimento.length > 0) {
+        let html = "";
+        dbAlimento.forEach((alimento, id) => {
+            html += "<tr>";
+            html += `<td>${alimento.nome}</td>`;
+            html += `<td>${alimento.categoria}</td>`;
+            html += `<td>${alimento.descricao}</td>`;
+            html += `<td>${alimento.restricao}</td>`;
+            html += `<td>${alimento.validade}</td>`;
+            html += `<td>${alimento.armazenamento}</td>`;
+            html += `<td><button class="botao vermelho" onclick="deleteAlimento(${id})">Excluir</button><button class="botao verde" onclick="window.location.assign('formulario_alimento.html?id=${id}')">Atualizar</button></td>`;
+            html += "</tr>";
+        });
+        corpoTabela.innerHTML = html;
+    }
 }
 
-const getLocalStorage = () => JSON.parse(localStorage.getItem('db_alimento')) ?? []
-const setLocalStorage = (db_alimento) => localStorage.setItem("db_alimento", JSON.stringify (db_alimento))
-
-const deleteAlimento = (index) => {
-    const db_alimento = readAlimento()
-    db_alimento.splice(index,1)
-    setLocalStorage(db_alimento)
+function deleteAlimento (index) {
+    dbAlimento.splice(index,1);
+    localStorage.setItem("dbAlimento", JSON.stringify(dbAlimento));
+    window.location.reload();
 }
 
-const updateAlimento = (index, alimento) => {
-    const db_alimento = readAlimento()
-    db_alimento[index] = alimento
-    setLocalStorage(db_alimento)
-}
-
-const readAlimento = () => getLocalStorage()
-
-const createAlimento = (alimento) => {
-    const db_alimento = getLocalStorage()
-    db_alimento.push (alimento)
-    setLocalStorage(db_alimento)
-}
+carregarItens();
--- js/formulario_alimento.js
+++ js/formulario_alimento.js
@@ -5,3 +5,33 @@ function validarSessao(){
 if (!validarSessao()) {
     window.location.assign('../index.html');
 }
+
+const formularioAlimento = document.getElementById('formulario-alimento');
+const idAlimento = new URLSearchParams(window.location.search).get('id');
+const dbAlimento = JSON.parse(localStorage.getItem("dbAlimento")) || [];
+
+if (idAlimento && dbAlimento[idAlimento]) {
+    const alimento = dbAlimento[idAlimento];
+    for (const key in alimento) {
+        if (formularioAlimento[key]) {
+            formularioAlimento[key].value = alimento[key];
+        }
+    }
+}
+
+formularioAlimento.addEventListener('submit', (event) => {
+    event.preventDefault();
+
+    const dadosFormulario = new FormData(formularioAlimento);
+    const alimentoAtualizado = Object.fromEntries(dadosFormulario.entries());
+
+    if (idAlimento !== null) {
+        dbAlimento[idAlimento] = alimentoAtualizado;
+    } else {
+        dbAlimento.push(alimentoAtualizado);
+    }
+
+    localStorage.setItem("dbAlimento", JSON.stringify(dbAlimento));
+
+    window.location.assign('index.html');
+});
